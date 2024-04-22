fdd3dec505c03b08a63a6e7746479a45b3f66bf3
wilenkristoffer <wilen.kristoffer@gmail.com>
2024-04-22 15:23:25 +0200

    Project initialized

---
+++ src/test/java/org/example/bokningssystem1/Bokningssystem1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bokningssystem1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bokningssystem1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
