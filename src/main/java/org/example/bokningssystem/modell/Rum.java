e143c15f22cd40bd170dd2ef858d7872cab57ace
wilenkristoffer <wilen.kristoffer@gmail.com>
2024-04-30 12:10:34 +0200

    added validation to rum/added developer terms.

--- src/main/java/org/example/bokningssystem/modell/Rum.java
+++ src/main/java/org/example/bokningssystem/modell/Rum.java
@@ -3,11 +3,14 @@ package org.example.bokningssystem.modell;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.NumberFormat;
 
 import java.util.List;
 
@@ -26,9 +29,13 @@ public class Rum {
     @Size(min = 3, message = "Minst 3 bokstäver")
     private String name;
 
+    @NotNull(message = "Rumstyp får inte vara null")
+    @NotEmpty
     @Enumerated(EnumType.STRING)
     private Rumstyp rumstyp;
 
+    @Size(max = 2, message = "Ska vara mellan 0 till 2")
+    @Positive(message = "Värdet måste vara positivt")
     private int antalSangar;
 
     @OneToMany(mappedBy = "room")
