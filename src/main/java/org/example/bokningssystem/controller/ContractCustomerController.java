07388e31bc34a7e4e45696de48501a36f883f3e0
wilenkristoffer <wilen.kristoffer@gmail.com>
2024-05-09 17:05:29 +0200

    made customerCompany a link to another page

--- src/main/java/org/example/bokningssystem/controller/ContractCustomerController.java
+++ src/main/java/org/example/bokningssystem/controller/ContractCustomerController.java
@@ -5,7 +5,9 @@ import org.example.bokningssystem.dtos.ContractCustomerDto;
 import org.example.bokningssystem.services.ContractCustomerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -24,5 +26,13 @@ public class ContractCustomerController {
         model.addAttribute("emptyListMessage", "Inga företagskunder hittades");
             return "contractCustomers.html";
         }
+    @GetMapping("/customer/details")
+    public String getCustomerDetails(Long customerId, RedirectAttributes redirectAttributes) {
+
+        redirectAttributes.addFlashAttribute("customerId", customerId);
+
+
+        return "customerDetails.html";
+    }
 }
 
