f9b6ed32ceeaea4be3bf05195d23f9c3e829bd00
Gashi87 <shayan_koyi@hotmail.com>
2024-05-09 12:43:57 +0200

    Removed comment to make it work

--- src/main/java/org/example/bokningssystem/controller/ContractCustomerController.java
+++ src/main/java/org/example/bokningssystem/controller/ContractCustomerController.java
@@ -1,22 +1,26 @@
 package org.example.bokningssystem.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.bokningssystem.dtos.ContractCustomerDto;
+import org.example.bokningssystem.services.ContractCustomerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class ContractCustomerController {
 
-    //private final ContractCustomerService contractCustomerService;
+    private final ContractCustomerService contractCustomerService;
 
     @RequestMapping("contractCustomers")
         public String contractCustomers(Model model){
 
-        //List<ContractCustomerDto> contractCustomers = contractCustomerService.getAllCustomersSimple();
+        List<ContractCustomerDto> contractCustomers = contractCustomerService.getAllCustomersSimple();
 
-       // model.addAttribute("contractCustomers", contractCustomers);
+        model.addAttribute("contractCustomers", contractCustomers);
         model.addAttribute("emptyListMessage", "Inga företagskunder hittades");
             return "contractCustomers.html";
         }
