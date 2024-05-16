a7a59e20045cc074e35136498423caee989c51a2
wilenkristoffer <wilen.kristoffer@gmail.com>
2024-05-16 20:09:41 +0200

    fixed files for eventhandlingFeature

--- src/main/java/org/example/bokningssystem/repo/EventRepo.java
+++ src/main/java/org/example/bokningssystem/repo/EventRepo.java
@@ -1,5 +1,7 @@
 package org.example.bokningssystem.repo;
 
+import org.example.bokningssystem.dtos.DetailedEventDto;
+import org.example.bokningssystem.dtos.DetailedRumDto;
 import org.example.bokningssystem.modell.Event;
 import org.example.bokningssystem.modell.Rum;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +9,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface EventRepo extends JpaRepository<Event, Long> {
-    List<Event> findByRum(Rum rum);
+    List<DetailedEventDto> findByRum(DetailedRumDto rum);
 }
